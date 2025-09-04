7a7c1df5c94f8c2be67b3a497c1963ccb78c5343
kritikamehra <kritikam1206@gmail.com>
2025-09-04 23:29:34 +0530

    Add TipCalculator

---
+++ TipCalculator/TipCalculator/ContentView.swift
@@ -0,0 +1,51 @@
+//
+//  ContentView.swift
+//  TipCalculator
+//
+//  Created by Kritika Mehra on 25/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var billAmount: Int = 0
+    var tips = [10, 15, 20, 25, 30]
+    @State var tipPercentage: Int = 0
+    @State var total = 0
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Bill Amount")
+                TextField("Enter Bill Amount", value: $billAmount, format: .number)
+            }
+            HStack {
+                Text("Tip Percentage: ")
+                Spacer()
+                Text("\(tipPercentage)%")
+                Picker("Tip Percentage: ", selection: $tipPercentage) {
+                    ForEach(tips, id: \.self) { option in
+                        Text("\(option)")
+                    }
+                }
+                .onChange(of: tipPercentage) {
+                    calculateTip()
+                }
+            }
+            Text("Total Bill: \(total)")
+                .font(.title)
+//                .numberFormatter(.currency)
+
+        }
+        .padding()
+    }
+    
+    func calculateTip() {
+        total = billAmount + (billAmount*tipPercentage/100)
+    }
+    
+    
+}
+
+#Preview {
+    ContentView()
+}
